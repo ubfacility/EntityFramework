5950751c532d5deb65f8bef97194356ae512e186
Smit Patel <smitpatel@users.noreply.github.com>
2016-10-21 20:13:32 -0700

    Query: Skip/Take with Any/All should preserve skip/take

--- src/Microsoft.EntityFrameworkCore.Relational/Query/Expressions/SelectExpression.cs
+++ src/Microsoft.EntityFrameworkCore.Relational/Query/Expressions/SelectExpression.cs
@@ -298,9 +298,10 @@ namespace Microsoft.EntityFrameworkCore.Query.Expressions
             [param: CanBeNull]
             set
             {
-                Check.NotNull(value, nameof(value));
-
-                PushDownIfLimit();
+                if (value != null)
+                {
+                    PushDownIfLimit();
+                }
 
                 _limit = value;
             }
